10f426ba1c9d329b2fea0ab730e350151031c253
Daniel Shaw <github@dshaw.com>
2010-10-30 11:38:26 -0400

    Fixed Storify unload when a tweet action is activated.

--- storify-twitter.js
+++ storify-twitter.js
@@ -1,7 +1,7 @@
 waitForjQuery();
 
 function waitForjQuery() {
-  if (typeof jQuery == "undefined") {
+  if (typeof jQuery == 'undefined') {
     setTimeout(waitForjQuery, 10000);
   } else {
     initStorify();
@@ -9,38 +9,40 @@ function waitForjQuery() {
 }
 
 function initStorify() {
+
   function setTarget(permalink) {
     return 'http://storify.com/import?permalink='+encodeURIComponent(permalink);
   }
 
   $('#page-container').delegate('.tweet', 'mouseover', function() {
-    var permalinkAttr = "data-storify-permalink";
-    if ( !$(this).attr(permalinkAttr) || !$(this).find('.storify-action') ) {
+    var permalinkAttr = 'data-storify-permalink';
+    if ( $(this).find('.storify-action').length === 0 || !$(this).attr(permalinkAttr) ) {
       var permalink = 'http://twitter.com/' + $(this).attr('data-screen-name')
                     + '/status/' + $(this).attr('data-tweet-id');
       $(this).attr(permalinkAttr, permalink);
       var target = setTarget(permalink);
       var storifyAction = $('<a href="' + target + '" class="storify-action" '
-                        + '><span><i></i><b>Storify</b></span></a>');
+                        + '><span><i></i><b>Storify</b></span></a>')
+          .click(function(e) {
+            e.preventDefault();
+            var w = 460,
+                h = 580,
+                sh = screen.height,
+                sw = screen.width,
+                top = 0;
+            var left = Math.round((sw/2)-(w/2));
+            if (sh>h) {
+              top = Math.round((sh/2)-(h/2))
+            }
+            var popup = window.open(target, 'storify_this','left='+left+',top='+top+',width='+w+',height='+h+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+            if (popup) {
+              popup.focus();
+            } else {
+              window.location.href=target;
+            }
+          });
       $(this).find('.tweet-actions').prepend(storifyAction);
-      storifyAction.click(function(e) {
-        event.preventDefault();
-        var w = 460,
-            h = 580,
-            sh = screen.height,
-            sw = screen.width,
-            top = 0;
-        var left = Math.round((sw/2)-(w/2));
-        if(sh>h){
-          top=Math.round((sh/2)-(h/2))
-        }
-        var popup = window.open(target,"storify_this","left="+left+",top="+top+",width="+w+",height="+h+",personalbar=0,toolbar=0,scrollbars=1,resizable=1");
-        if (popup) {
-          popup.focus();
-        } else {
-          window.location.href=target;
-        }
-      });
     }
   });
+
 }
