d685bca5cf2d1671771c08fb1f3e81920803dc53
xdamman <xdamman@storify.com>
2011-06-15 18:33:30 -0700

    Added support for fb comments on fan pages.

--- storify-facebook.js
+++ storify-facebook.js
@@ -15,12 +15,30 @@ jQuery(document).ready(function($) {
     return target;
   };
 
-  var storifyThis = function() {
-    var elem = $(this);
+  var storifyThis = function(elementType) {
+    var elem = $(this), storyElement;
 
-    if ( !elem.hasClass('storifyed') ) {
+    console.log("Storifying element "+elementType,this);
 
-      var storyElement = {
+    if ( elem.hasClass('storifyed') ) 
+      return;
+      
+    if(elem.hasClass('uiUfiComment')) {
+      storyElement = {
+        description: elem.find(".commentContent span").text(),
+        elementClass: "fbpost",
+        created_at: elem.find("abbr").attr("data-date"),
+        author: {
+          name: elem.find("a.actorName").text(),
+          href: elem.find("a.actorName").attr("href"),
+          avatar: elem.find("img.uiProfilePhoto").attr("src")
+        }
+      };
+      if(storyElement.author.href)
+        storyElement.permalink = window.location.href+'#'+storyElement.author.href.replace(/https?:\/\/(www\.)?facebook\.com\//i,'');
+    }
+    else {
+      storyElement = {
         description: elem.find(".UIStory_Message,.messageBody").html(),
         elementClass: "fbpost",
         created_at: elem.find("abbr").attr("data-date"),
@@ -31,7 +49,6 @@ jQuery(document).ready(function($) {
         },
         metadata: JSON.parse(elem.attr("data-ft"))
       };
-
       var slug = elem.find(".UIIntentionalStory_Time a, .uiStreamSource a").attr("href");
       if (slug) {
         if (slug.charAt(0) == '/') {
@@ -49,38 +66,39 @@ jQuery(document).ready(function($) {
           href: elem.find(".uiStreamAttachments .uiAttachmentTitle a").attr("href")
         };
       }
-
-      var storyElement_str = JSON.stringify(storyElement),
-          target = setTarget(storyElement.permalink, storyElement_str);
-
-      var storifyAction = $('<a/>', {
-          href: '',
-          class: 'storify-action',
-          'data-storyElement': storyElement_str,
-          html: '<span><i></i>Storify</span>',
-          click: function(e) {
-            e.preventDefault();
-            var w = 460,
-                h = 580,
-                sh = screen.height,
-                sw = screen.width,
-                top = 0;
-            var left = Math.round((sw/2)-(w/2));
-            if (sh>h) {
-              top = Math.round((sh/2)-(h/2))
-            }
-            window.open(target, 'storify_this','left='+left+',top='+top+',width='+w+',height='+h+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
-          }
-        });
-
-      elem
-          .addClass('storifyed')
-          .find('.UIActionLinks').prepend(' · ', storifyAction);
     }
-  };
+
+    var storyElement_str = JSON.stringify(storyElement),
+        target = setTarget(storyElement.permalink, storyElement_str);
+
+    var storifyAction = $('<a/>', {
+        href: '',
+        class: 'storify-action',
+        'data-storyElement': storyElement_str,
+        html: '<span><i></i>Storify</span>',
+        click: function(e) {
+          e.preventDefault();
+          var w = 460,
+              h = 580,
+              sh = screen.height,
+              sw = screen.width,
+              top = 0;
+          var left = Math.round((sw/2)-(w/2));
+          if (sh>h) {
+            top = Math.round((sh/2)-(h/2))
+          }
+          window.open(target, 'storify_this','left='+left+',top='+top+',width='+w+',height='+h+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+        }
+      });
+
+    elem.addClass('storifyed');
+    if(elem.hasClass('uiUfiComment')) elem.find('.commentActions').append(' · ', storifyAction);
+    else elem.find('.UIActionLinks').prepend(' · ', storifyAction);
+  }
+  
 
   // initialize
-  $('.UIIntentionalStory, .uiStreamStory, .mall_post').each(storifyThis);
+  $('.UIIntentionalStory, .uiStreamStory, .mall_post, .uiUfiComment').each(storifyThis);
 
   /**
    * Continue storifying feed items
